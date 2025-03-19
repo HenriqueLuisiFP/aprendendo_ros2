2299a0fe0bb02aaeeb9aad53ad27867d5474bfa7
HenriqueLuisiFP <henrique.luisi2002@gmail.com>
2025-03-19 18:51:29 -0300

    Projeto1 FinalCompleto
    Codigo corrigido e funcional

--- workspace/src/projeto1/projeto1/posicao.py
+++ workspace/src/projeto1/projeto1/posicao.py
@@ -2,8 +2,10 @@ import rclpy
 from rclpy.node import Node
 from rclpy.qos import QoSProfile, QoSReliabilityPolicy
 from sensor_msgs.msg import JointState, LaserScan
-from std_msgs.msg import Header
+from std_msgs.msg import Header, Float64
 import time
+import numpy as np
+import random
 
 class NoDePosicao(Node):
 
@@ -15,17 +17,31 @@ class NoDePosicao(Node):
         # Subscribers
         self.subscription_joint = self.create_subscription(
             JointState, '/joint_states', self.listener_callback_joint, qos_profile)
-
+        
         self.subscription_laser = self.create_subscription(
             LaserScan, '/scan', self.listener_callback_laser, qos_profile)
 
         # Publisher
         self.publisher = self.create_publisher(JointState, '/joint_state', qos_profile)
+        self.publisher_x = self.create_publisher(Float64, '/robot_x', qos_profile)
 
-        # Timer to publish messages every 0.5 seconds
-        self.timer = self.create_timer(0.5, self.talker_callback_joint)
+        
+        self.timer = self.create_timer(0.5, self.update_position)
 
-        # Initialize joint positions
+        # Constantes
+        self.raio = 0.033 
+        self.distancia_rodas = 0.178 
+
+        # Variaveis
+        self.posicao = [0.0, 0.0, 0.0]
+        self.medidas = [None, None]  
+        self.ultimas_medidas = [None, None]
+
+        self.distancias = [0, 0]
+        self.sigma_odometria = 0.2   
+        self.sigma_lidar = 0.175   
+        self.sigma_movimento = 0.002   
+
         self.jointL = 0.0
         self.jointR = 0.0
 
@@ -33,30 +49,59 @@ class NoDePosicao(Node):
         rclpy.spin(self)
 
     def listener_callback_joint(self, msg):
+       
         self.jointL = msg.position[0]
         self.jointR = msg.position[1]
 
     def listener_callback_laser(self, msg):
-        self.laser = msg.ranges[80]
+     
+        self.laser = msg.ranges[72] 
 
-    def talker_callback_joint(self):
-        msg = JointState()
+    def update_position(self):
+ 
+        if self.ultimas_medidas[0] is None or self.ultimas_medidas[1] is None:
+            self.ultimas_medidas[0] = self.jointL
+            self.ultimas_medidas[1] = self.jointR
+            return
         
-        # 🔥 FIX: Set Header (Mandatory for Some Subscribers)
-        msg.header = Header()
-        msg.header.stamp = self.get_clock().now().to_msg()
-        msg.header.frame_id = "base_link"  # Change if needed
+       
+        self.medidas[0] = self.jointL
+        self.medidas[1] = self.jointR
 
-        msg.position = [self.jointL, self.jointR]
+        #
+        diff_left = self.medidas[0] - self.ultimas_medidas[0]
+        self.distancias[0] = diff_left * self.raio + random.random() * 0.002
+        self.ultimas_medidas[0] = self.medidas[0]
+
+        diff_right = self.medidas[1] - self.ultimas_medidas[1]
+        self.distancias[1] = diff_right * self.raio + random.random() * 0.002
+        self.ultimas_medidas[1] = self.medidas[1]
+
+      
+        deltaS = (self.distancias[0] + self.distancias[1]) / 2.0
+        deltaTheta = (self.distancias[1] - self.distancias[0]) / self.distancia_rodas
+        self.posicao[2] = (self.posicao[2] + deltaTheta) % (2 * np.pi)
+
+       
+        deltaSx = deltaS * np.cos(self.posicao[2])
+        deltaSy = deltaS * np.sin(self.posicao[2])
+
+        self.posicao[0] += deltaSx
+        self.posicao[1] += deltaSy
+        self.pub_x(self.posicao[0])
+
+
+    def pub_x(self, x_position):
         
-
-        self.publisher.publish(msg)
-        self.get_logger().info(f'Published JointState: {msg.position}')
+        msg = Float64()
+        msg.data = x_position
+        self.publisher_x.publish(msg)
+        self.get_logger().info(f'X: {x_position}')
 
     def __del__(self):
         self.get_logger().info('Finalizando o nó! Tchau, tchau...')
 
-# Main function
+
 def main(args=None):
     rclpy.init(args=args)
     node = NoDePosicao()
@@ -70,5 +115,3 @@ def main(args=None):
 
 if __name__ == '__main__':
     main()
-
-
